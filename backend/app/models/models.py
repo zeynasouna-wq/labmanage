d3b53ad5c2306bcbe4face83fa84ccbddf67ed07
zeynasouna-wq <zeynasouna@gmail.com>
2026-04-17 13:23:00 +0000

    fix" fixing many bugs for cors blocking

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -158,7 +158,7 @@ class StockMovement(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
-    lot_id = Column(Integer, ForeignKey("product_lots.id"), nullable=False)  # OBLIGATOIRE
+    lot_id = Column(Integer, ForeignKey("product_lots.id"), nullable=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
     movement_type = Column(SAEnum(MovementType), nullable=False)
